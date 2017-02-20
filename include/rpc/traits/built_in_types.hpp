f3ab2cb4c5af66bd0ce20eb49b9cabafae45299c
MadNiko <for-niko@yandex.ru>
2017-02-20 23:04:02 +0300

    добавление функций конверирования для std::vector и std::(w)string

--- include/rpc/traits/built_in_types.hpp
+++ include/rpc/traits/built_in_types.hpp
@@ -114,6 +114,55 @@ unzip(value_t& value, const rpc::byte* in, const rpc::byte* in_end)
 }
 
 
+
+//======================================================================================================================
+// vector
+//----------------------------------------------------------------------------------------------------------------------
+
+template <
+    class item,
+    class allocator = std::allocator<item>,
+    class vector = std::vector<item, allocator>
+>
+static void zip(buffer_bytes& buffer, vector&& value)
+{
+    const rpc::buffer_bytes::size_type size_size_vec(sizeof(typename vector::size_type));
+    typename vector::size_type size_vec(value.size());
+
+    buffer.reserve(buffer.size() + size_size_vec + (size_vec * sizeof(typename vector::value_type)));
+
+    rpc::traits::types::zip(buffer, std::move(size_vec));
+    for (typename vector::value_type& item : value)
+    {
+        rpc::traits::types::zip(buffer, std::move(item));
+    }
+}
+template <
+    class item,
+    class allocator = std::allocator<item>,
+    class vector = std::vector<item, allocator>
+>
+static const rpc::byte* unzip(vector& value, const rpc::byte* in, const rpc::byte* in_end)
+{
+    typename vector::size_type size_vec(0);
+    in = rpc::traits::types::unzip(size_vec, in, in_end);
+    if (in == nullptr)
+        return nullptr;
+
+    value.resize(size_vec);
+
+    for (typename vector::value_type& item : value)
+    {
+        in = rpc::traits::types::unzip(item, in, in_end);
+        if (in == nullptr)
+            return nullptr;
+    }
+
+    return in;
+}
+
+
+
 //======================================================================================================================
 // bool
 //----------------------------------------------------------------------------------------------------------------------
@@ -136,6 +185,90 @@ unzip(value_t& value, const rpc::byte* in, const rpc::byte* in_end)
         value = (0 != tmp_value);
     return in_next;
 }
+
+
+
+//======================================================================================================================
+// string's
+//----------------------------------------------------------------------------------------------------------------------
+namespace impl
+{
+
+template<
+    class elem,
+    class traits = std::char_traits<elem>,
+    class alloc = std::allocator<elem>,
+    class string = std::basic_string<elem, traits, alloc>
+>
+inline void zip(rpc::buffer_bytes& buffer, string&& str)
+{
+    const rpc::buffer_bytes::size_type size_size_str(sizeof(typename string::size_type));
+    typename string::size_type size_str(str.size());
+
+    buffer.reserve(buffer.size() + size_size_str + (size_str * sizeof(typename string::value_type)));
+
+    rpc::traits::types::zip(buffer, std::move(size_str));
+    for (typename string::value_type& value : str)
+    {
+        rpc::traits::types::zip(buffer, std::move(value));
+    }
+}
+
+template<
+    class elem,
+    class traits = std::char_traits<elem>,
+    class alloc = std::allocator<elem>,
+    class string = std::basic_string<elem, traits, alloc>
+>
+inline const rpc::byte* unzip(string& str, const rpc::byte* in, const rpc::byte* in_end)
+{
+    typename string::size_type size_str(0);
+    in = rpc::traits::types::unzip(size_str, in, in_end);
+    if (in == nullptr)
+        return nullptr;
+
+    str.resize(size_str);
+
+    for (typename string::value_type& value : str)
+    {
+        in = rpc::traits::types::unzip(value, in, in_end);
+        if (in == nullptr)
+            return nullptr;
+    }
+
+    return in;
+}
+
+} // namespace impl
+
+static void zip(rpc::buffer_bytes& buffer, std::string&& str)
+{
+    rpc::traits::types::impl::zip<char>(buffer, std::move(str));
+}
+static void zip(rpc::buffer_bytes& buffer, std::wstring&& str)
+{
+    rpc::traits::types::impl::zip<wchar_t>(buffer, std::move(str));
+}
+
+static const rpc::byte* unzip(std::string& str, const rpc::byte* in, const rpc::byte* in_end)
+{
+    return rpc::traits::types::impl::unzip<char>(str, in, in_end);
+}
+static const rpc::byte* unzip(std::wstring& str, const rpc::byte* in, const rpc::byte* in_end)
+{
+    return rpc::traits::types::impl::unzip<wchar_t>(str, in, in_end);
+}
+
+
+
+//======================================================================================================================
+// tuple
+//----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
 
 
 } // namespace types
